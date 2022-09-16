25d8326ecd71b89eda8c85408ed6a2beca3f3574
Edita2 <edita.bark@icloud.com>
2022-09-16 11:59:11 +0300

    added footer with clickable logos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import MainPage from "./Components/MainPage/MainPage";
 import styles from "./global.css";
+import Footer from "../src/Components/Footer/Footer";
 
 function App() {
   return (
     <div className={styles.content}>
       <Fragment>
         <MainPage />
+        <Footer />
       </Fragment>
     </div>
   );
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,27 @@
+import logogithub from "./../../Logos/images/logo-github.svg";
+import logolinkedin from "../../Logos/images/logo-linkedin.svg";
+import classes from "./Footer.module.css";
+
 const Footer = () => {
-  <div>
-    <Card></Card>
-  </div>;
+  return (
+    <div className={classes.footer}>
+      <p>Copyright @ 2022. Powered by Edita Barkauskaitė </p>
+      <a
+        href="https://github.com/Edita2"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <img className={classes.logo} src={logogithub} alt="logo-github" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/edita-barkauskait%C4%97/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <img className={classes.logo} src={logolinkedin} alt="logolinkedin" />
+      </a>
+    </div>
+  );
 };
+
+export default Footer;
