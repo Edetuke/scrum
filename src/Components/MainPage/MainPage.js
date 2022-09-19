d7b3e048a13379bb278701982d3c6eab0bc0c27f
Edita2 <edita.bark@icloud.com>
2022-09-19 16:33:12 +0300

    updated structure

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -2,14 +2,22 @@ import React from "react";
 import { Fragment } from "react";
 import MainNavigation from "../Navigation/MainNavigation";
 import AboutCapacity from "./AboutCapacity";
-
+import About from "./About";
 import AboutFacilitator from "./AboutFacilitator";
 import AboutPoker from "./AboutPoker";
+import Backroundimg from "../../Logos/images/about-backroud.jpg";
+import classes from "./MainPage.module.css";
 
 const MainPage = () => {
   return (
     <Fragment>
+      <img
+        className={classes.backroundimg}
+        src={Backroundimg}
+        alt="backround-img"
+      />
       <MainNavigation />
+      <About />
       <AboutFacilitator />
       <AboutCapacity />
       <AboutPoker />
--- src/Components/Navigation/MainNavigation.js
+++ src/Components/Navigation/MainNavigation.js
@@ -1,19 +1,31 @@
 import React from "react";
-import { Fragment } from "react";
 import logo from "../../Logos/screasy-logo.svg";
 import classes from "../Navigation/MainNavigation.module.css";
-import MenuNav from "./Menu";
-import AboutBackround from "../../Logos/images/about-backroud.jpg";
-import About from "../MainPage/About";
+import { Link } from "react-router-dom";
 
 const MainNavigation = () => {
   return (
-    <Fragment>
-      <img className={classes.backround} src={AboutBackround} alt="backround" />
+    <div className={classes.navigation}>
       <img className={classes.logo} src={logo} alt="logo" />
-      <MenuNav />
-      <About />
-    </Fragment>
+      <div className={classes.menu}>
+        <Link to="/about" className={classes.link}>
+          {" "}
+          About
+        </Link>
+        <Link to="/fasilitator" className={classes.link}>
+          {" "}
+          Fasilitator election{" "}
+        </Link>
+        <Link to="/capacity" className={classes.link}>
+          {" "}
+          capacity calculator{" "}
+        </Link>
+        <Link to="/poker" className={classes.link}>
+          {" "}
+          story point poker{" "}
+        </Link>
+      </div>
+    </div>
   );
 };
 
