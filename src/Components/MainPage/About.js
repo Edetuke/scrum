844e3b99d4f05d6e1ee4f3fff3155f4a4b8f6148
Edita2 <edita.bark@icloud.com>
2022-09-20 14:40:22 +0300

    added button navigation

--- src/Components/MainPage/About.js
+++ src/Components/MainPage/About.js
@@ -1,7 +1,14 @@
 import React from "react";
 import classes from "./About.module.css";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/fasilitator-election`;
+    navigate(path);
+  };
+
   return (
     <div className={classes.about}>
       <h2>One page to simplify your scrum process</h2>
@@ -10,7 +17,9 @@ const About = () => {
         to faster your scrum process.
       </p>
 
-      <button className={classes.button}>Try facilitator election</button>
+      <button className={classes.button} onClick={routeChange}>
+        Try facilitator election
+      </button>
     </div>
   );
 };
