47dcf8d80889f0fb2142e600db65bf03aa6da5ad
Edita2 <edita.bark@icloud.com>
2022-09-27 23:30:12 +0300

    updated navigation styles

--- src/Components/Navigation/MainNavigation.js
+++ src/Components/Navigation/MainNavigation.js
@@ -7,6 +7,22 @@ const MainNavigation = (props) => {
   return (
     <div className={classes.navigation}>
       <img className={classes.logo} src={logo} alt="logo" />
+      <button className={classes.button}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={classes.icon}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+          />
+        </svg>
+      </button>
       <div className={classes.menu}>
         <NavLink to="/about" className={classes.link}>
           About
@@ -32,22 +48,6 @@ const MainNavigation = (props) => {
         >
           story point poker
         </NavLink>
-        <button className={classes.button}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className={classes.icon}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-            />
-          </svg>
-        </button>
       </div>
     </div>
   );
