1aaa3148a6ee28b7e008963bd6a6407d2a746a31
Edita2 <edita.bark@icloud.com>
2022-09-27 23:18:02 +0300

    added input form validation

--- src/Components/ModalForm.js
+++ src/Components/ModalForm.js
@@ -1,12 +1,17 @@
 import Modal from "./Modal";
+import { useState } from "react";
 
 const ModalForm = (props) => {
+  const [name, setName] = useState();
+
   return (
     <Modal onClose={props.onClose}>
       <h2>Enter your user name</h2>
-      <input type="text" />
-      <button onClick={props.onClose}>Save</button>
-      <button onClick={props.onClose}>Close</button>
+      <input type="text" onChange={(e) => setName(e.target.value.trim())} />
+      <button disabled={!name} onClick={props.onClose}>
+        Save
+      </button>
+      <button onClick={props.onClose}>Cancel</button>
     </Modal>
   );
 };
--- src/Components/Navigation/MainNavigation.js
+++ src/Components/Navigation/MainNavigation.js
@@ -18,12 +18,36 @@ const MainNavigation = (props) => {
         >
           Fasilitator election
         </NavLink>
-        <NavLink to="/capacity-calculator" className={classes.link}>
+        <NavLink
+          to="/capacity-calculator"
+          className={classes.link}
+          onClick={props.onShowModal}
+        >
           capacity calculator
         </NavLink>
-        <NavLink to="/poker" className={classes.link}>
+        <NavLink
+          to="/poker"
+          className={classes.link}
+          onClick={props.onShowModal}
+        >
           story point poker
         </NavLink>
+        <button className={classes.button}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={classes.icon}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
